b562c14186303dc9bd0037cd1971e1cee7d8ab16
dciangot <diego.ciangottini@gmail.com>
2021-05-17 11:30:14 +0200

    new images

---
+++ stable/jhub-aas/docker/spawn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Configure oidc-agent for user token management
+echo "eval \`oidc-keychain\`" >> ~/.bashrc
+eval `oidc-keychain`
+oidc-gen dodas --issuer $IAM_SERVER \
+               --client-id $IAM_CLIENT_ID \
+               --client-secret $IAM_CLIENT_SECRET \
+               --rt $REFRESH_TOKEN \
+               --confirm-yes \
+               --scope "openid profile email" \
+               --redirect-uri  http://localhost:8843 \
+               --pw-cmd "echo \"DUMMY PWD\""
